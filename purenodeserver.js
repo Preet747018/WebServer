0b039917454ed3a7d325d9612fd787989da20f31
Lovepreet Kaur <747018@student.tor.loc>
2019-07-31 15:02:32 -0400

    fifth commit

--- purenodeserver.js
+++ purenodeserver.js
@@ -1,10 +1,13 @@
 const http = require('http');
 
 function handler(req, res){
+const parseUrl = url.parse(req.url, true);
+console.log(parseUrl);
 res.writeHead(200,{'Content-type':'text/plane'});
 res.write("Hello, I am a webServer");
+res.end();
 }
 
 const server = http.createServer();
 
-server.listen(3000);
+server.listen(3200);
