7a80b58e8672105d48582a2d318c144394f9ae9f
Lovepreet Kaur <747018@student.tor.loc>
2019-07-31 14:14:50 -0400

    third commit

--- purenodeserver.js
+++ purenodeserver.js
@@ -1,3 +1,7 @@
 const http = require('http');
 
-const server = http.createServer():
+function handler(req, res){
+res.writeHead(200,{'Content-type':'text/plane'});
+}
+
+const server = http.createServer();
