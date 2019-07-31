a149cae53883bc34ae31c474e1c1a72ccfefe5b5
Lovepreet Kaur <747018@student.tor.loc>
2019-07-31 15:56:13 -0400

    Seventh commit

--- purenodeserver.js
+++ purenodeserver.js
@@ -4,9 +4,15 @@ const url = require('url');
 function handler(req, res){
 const parseUrl = url.parse(req.url, true);
 console.log(parseUrl);
+
+if(parseUrl.pathname ==='/'){
 res.writeHead(200,{'Content-type':'text/plane'});
-res.write("Hello, I am a webServer");
+res.write("Yeah, Go Server!!");
 res.end();
+} else{
+    res.writeHead(404,{'Content-type':'text/plain'});
+}
+
 }
 
 const server = http.createServer(handler);
